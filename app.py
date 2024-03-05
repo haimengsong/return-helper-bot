55965bb71f1fb7f73db5b8f790fc24cd54c3f420
i5397807 <herman.song@sap.com>
2024-03-05 15:05:12 +0800

    fix pdf issue

--- app.py
+++ app.py
@@ -28,21 +28,14 @@ def upload_pdf():
     st.sidebar.title("请选择PDF文件")
 
     # 在侧边栏创建一个文件上传器，限制文件类型为PDF，且一次只能上传一个文件
+    global pdf_list
     pdf_list = st.sidebar.file_uploader("一次性选择一个PDF文件", type="pdf", accept_multiple_files=False)
     # 如果用户已经上传了文件，就在侧边栏显示一个消息
     if pdf_list is not None:
+        initialize_db(pdf_list)
         st.sidebar.write("文件载入成功，现在可以进行文档问答")
 
     return pdf_list
-
-def get_pdf_text(pdf_path):
-    text = ""
-    pdf_file = open(pdf_path, 'rb')
-    pdf_reader = PdfReader(pdf_file)
-    for page in pdf_reader.pages:
-        text += page.extract_text()
-
-    return text
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
@@ -59,22 +52,26 @@ def save_vector_store(textChunks):
     db = FAISS.from_texts(textChunks, AzureOpenAIEmbeddings())
     db.save_local('faiss')
     return db
-def initialize_db():
-    global pdf_list
-    pdf_list = upload_pdf()
-    raw_text = get_pdf_text(pdf_list)
-    text_chunks = get_text_chunks(raw_text)
+def initialize_db(pdf_list):
+    text = ""
+    # 使用 PyPDF2 的 PdfReader 读取 pdf 文件
+    pdf_reader = PdfReader(pdf_list)
+
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+
+    text_chunks = get_text_chunks(text)
 
     db = save_vector_store(text_chunks)
     llm = AzureChatOpenAI(model_name="gpt-35-turbo", temperature=0.5)
 
-    global RETURN_HELPER_BOT
-    RETURN_HELPER_BOT = RetrievalQA.from_chain_type(llm,
+    global PDF_QA
+    PDF_QA = RetrievalQA.from_chain_type(llm,
                                                     retriever=db.as_retriever(search_type="similarity_score_threshold",
                                                                               search_kwargs={"score_threshold": 0.7}))
-    RETURN_HELPER_BOT.return_source_documents = True
+    PDF_QA.return_source_documents = True
 
-    return RETURN_HELPER_BOT
+    return PDF_QA
 
 def load_vector_store():
     return FAISS.load_local('faiss', AzureOpenAIEmbeddings())
@@ -84,7 +81,7 @@ def chat(message, history):
     print(f"[history]{history}")
     enable_chat = True
 
-    ans = RETURN_HELPER_BOT({"query": message})
+    ans = PDF_QA({"query": message})
     if ans["source_documents"] or enable_chat:
         print(f"[result]{ans['result']}")
         print(f"[source_documents]{ans['source_documents']}")
@@ -109,7 +106,7 @@ def launch_ui():
             st.markdown(message["content"])
 
     if user_input := st.chat_input("Ask your question: "):
-        if pdf_list is not None:
+        if pdf_list is not None and os.environ["AZURE_OPENAI_API_KEY"] is not None:
             # 在会话状态的消息列表中添加用户的输入
             st.session_state.messages.append({"role": "user", "content": user_input})
             # 在聊天窗口中显示用户的输入
@@ -121,6 +118,14 @@ def launch_ui():
                 message_placeholder.markdown(full_response)
                 # 在会话状态的消息列表中添加助手的回答
                 st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+        if pdf_list is None:
+            message_placeholder = st.empty()
+            message_placeholder.markdown("请先上传PDF文件..")
+
+        if os.environ["AZURE_OPENAI_API_KEY"] is None:
+            message_placeholder = st.empty()
+            message_placeholder.markdown("请先输入API Key..")
 
     if len(st.session_state.messages) > 20:
         st.session_state.messages = st.session_state.messages[-20]
@@ -134,5 +139,4 @@ if __name__ == "__main__":
     # env_path = os.getenv("HOME") + "/Downloads/.env"
     # load_dotenv(dotenv_path=env_path, verbose=True)
     set_api_key()
-    initialize_db()
     launch_ui()
